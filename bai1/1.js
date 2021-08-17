7d7047aeea9caf4fb1e8b60843e5b0bbb2562974
truongcpqn2421998 <truongcpqn24219982@gmail.com>
2021-08-17 13:59:50 +0700

    update

---
+++ bai1/1.js
@@ -0,0 +1,16 @@
+function No(){
+    console.log('Xin chào')
+    // alert('Gà')
+    document.getElementById("1").innerText="+1 con gà"
+}
+function Yes(){
+    document.getElementById("1").innerText="zo"
+}
+function YS(){
+    document.getElementById("1").innerText=""
+}
+function K2K(){
+    a=prompt("Nhập dữ liệu");
+    document.getElementById("1").innerText=a
+
+}
